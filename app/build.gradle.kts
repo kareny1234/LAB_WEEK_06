2d36697d785f7959cb7b8665b5d97fb2d0ef88ce
karen <karenyap222@gmail.com>
2025-10-15 22:31:48 +0700

    Commit No.4 - Assignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.github.bumptech.glide:glide:4.14.2")
 }
